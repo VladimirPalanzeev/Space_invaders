c0a577fdfb6608117cd182986ff09372f25252a0
VladimirPalanzeev <palanzey@gmail.com>
2022-03-07 14:46:32 +0500

    [upd] В образовательных целях решил полностью переписать код сначала определяю функции

---
+++ Space_invaders.py
@@ -0,0 +1,141 @@
+from tkinter import *
+from random import randint
+from time import sleep
+
+# Очистить всё и начать игру заново
+def continieAfterPause():
+    pass
+
+# Запись очков в файл
+def endTableScore(inputWindow, positionPlayer):
+    pass
+
+# Фильтрация вводимых знаков
+def inputNameFilner(event):
+    pass
+
+# Окно для ввода имени
+def getPlayerName(positionPlayer):
+    pass
+
+# Находим номер игрока в списке лучших
+def sortScoreTable(score):
+    pass
+
+# Конец игры
+def endGame():
+    pass
+
+# Геттер - координата X инопланетянина obj
+def getInvadersX(obj):
+    pass
+
+# Геттер - координата Y инопланетянина obj
+def getInvadersY(obj):
+    pass
+
+# Геттер - координата X корабля игрока
+def getPlayerX():
+    pass
+
+# Геттер - координата Y корабля игрока
+def gePlayerY():
+    pass
+
+# Геттер - координата X ракеты
+def getRocketX():
+    pass
+
+# Геттер - координата Y ракеты
+def getRocketY():
+    pass
+
+# Обновляем инфостроку
+def updateInfoLine():
+    pass
+
+# Запись очков в файл
+def saveScores(scoresToFile):
+    pass
+
+# Загрузка очков из scores.dat
+def loadScores():
+    pass
+
+# Удаляем таблицу очков
+def hideScores():
+    pass
+
+# Рисуем таблицу очков
+def showScores(numberPlayer):
+    pass
+
+# Показываем кнопки меню
+def showMenu():
+    pass
+
+# Скрываем кнопки меню
+def hideMenu():
+    pass
+
+# Анимация вражеской ракеты
+def animationInvadersRocket():
+    pass
+
+# Стартуем ракету врага
+def startInvadersRocket():
+    pass
+
+# Анимация взрыва
+def animationExplosion(frame, x, y):
+    pass
+
+# Старт анимации взрыва
+def startExplosion(n):
+    pass
+
+# Анимация ракеты
+def animationShot(frame):
+    pass
+
+# При нажатии на пробел выстрел
+def shoot():
+    pass
+
+# Перемещение игрока
+def move(x):
+    pass
+
+# Переключение на следующий уровень
+def nextLevel():
+    pass
+
+# Конец уровня
+def endLevel():
+    pass
+
+# Главный цикл игры
+def mainloop():
+    pass
+
+# Нажатие на кнопку старт
+def startGame():
+    pass
+
+# Сброс всего под чистую с установкой первого уровня
+def globalReset():
+    pass
+
+# Перезапуск игры полностью
+def restartGame():
+    pass
+
+# Сброс и формирование объектов игрового мира
+def reset():
+    pass
+
+# ++++++++++++++++++++ Основной блок программы +++++++++++++++
+
+
+
+
